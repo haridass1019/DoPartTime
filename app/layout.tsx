7c432b10726810ba2119416da76c682679903923
haridass <haridass@usistech.com>
2024-05-13 18:51:06 +0530

    layout added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Sidebar from "./components/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} page-wrapper`}>
+      {/* <div className="sidebar flex"> */}
+      
+          <Sidebar />
+        {/* </div> */}
+
+        <main className="flex-1 overflow-y-auto" style={{ backgroundColor: '#F2F2F4' }}>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -251,6 +251,132 @@
 
 // Server side api fetching start 
 
+// import { db } from './firbaseconfig';
+// import { collection, getDocs } from 'firebase/firestore';
+// import React, { useEffect, useState } from 'react';
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import { useRouter } from 'next/navigation';
+// import { FormEvent } from 'react'
+// const getData = async () => {
+//   try {
+//     const querySnapshot = await getDocs(collection(db, "jobs"));
+//     const data: any = [];
+//     querySnapshot.forEach((doc) => {
+//       data.push({ id: doc.id, ...doc.data() });
+//     });
+//     return data;
+//   } catch (error: any) {
+//     throw new Error("Failed to fetch data from Firestore: " + error.message);
+//   }
+// }
+// function formatTimeRange(start_time: any, end_time: any) {
+//   if (!start_time || !start_time.seconds || !end_time || !end_time.seconds) {
+//     return "Time not mentioned";
+//   }
+//   const options: any = { hour: 'numeric', minute: 'numeric', hour12: true };
+//   const startDate = new Date(start_time.seconds * 1000);
+//   const endDate = new Date(end_time.seconds * 1000);
+
+//   const formattedStartTime = startDate.toLocaleTimeString('en-US', options);
+//   const formattedEndTime = endDate.toLocaleTimeString('en-US', options);
+
+//   return `${formattedStartTime} - ${formattedEndTime}`;
+// }
+
+// function formatPostedTime(publish_time: any) {
+//   const timestamp = publish_time;
+//   const milliseconds = timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000;
+//   const date = new Date(milliseconds);
+//   const postDate = new Date(date);
+//   const currentDate = new Date();
+//   const timeDifference = currentDate.getTime() - postDate.getTime();
+//   const daysDifference = Math.floor(timeDifference / (1000 * 3600 * 24));
+//   if (daysDifference === 0) {
+//     return "Posted today";
+//   } else if (daysDifference === 1) {
+//     return "Posted 1 day ago";
+//   } else {
+//     return `Posted ${daysDifference} days ago`;
+//   }
+// }
+// const dashboard: any = async () => {
+//   try {
+//     const apiData = await getData();
+//     return (
+//       <>  
+//       <div className='centerpage'>
+//         <h1 className="text-center ">Firebase Data</h1>
+//         <h1 className="text-center p-10"> <Link href="/Pages/Profile">Profile</Link></h1>
+//         <div className="text-center ">
+//           <form action="/search/">
+//             <input
+//               type="text"
+//               name='search'
+//               placeholder="Search"
+//             />
+//             <button type='submit'>Search</button>
+
+//           </form>
+
+//           {apiData.map((item: any, index: any) => (
+//             <div key={index}>
+//               <div className="job-wrpper" key={item.id}>
+//                 <div className="job-wrpper" key={item.id}>
+//                   <Link href={`Pages/Jobs/Job-details/${item.id}`}>
+//                     <div className="job-header">
+//                       {item.image ? (
+//                         <Image src={item.image} width={32} height={32} alt="Company" />
+//                       ) : (
+//                         <Image src="https://flowbite.com/docs/images/logo.svg" width={32} height={32} alt="Default Company Logo" />
+//                       )}
+//                       <div>
+//                         <p>{item.title}</p>
+//                         <span>{item.title}</span>
+//                       </div>
+//                     </div>
+//                     <div className="job-body">
+//                       <Image src="/icon/map-pin.svg" width={16} height={16} alt="Logo" />
+//                       <span> {item.location}</span>
+//                     </div>
+//                     <div className="job-body">
+//                       <Image src="/icon/clock.svg" width={16} height={16} alt="Logo" />
+//                       <span>{formatTimeRange(item.start_time, item.end_time)}  {item.working_days}</span>
+//                     </div>
+//                     <div className="job-body">
+//                       <Image src="/icon/wallet.svg" width={16} height={16} alt="Logo" />
+//                       <span>₹ {item.start_salary} - {item.end_salary} per month</span>
+//                     </div>
+//                     <div className="postby">
+//                       <span>{formatPostedTime(item.publish_time)}</span>
+//                     </div>
+//                   </Link>
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//         </div>
+//       </>
+//     );
+//   } catch (error: any) {
+//     console.error(error);
+//     return (
+//       <>
+//         <h2>Profile</h2>
+//         <div>Error fetching data: {error.message}</div>
+//       </>
+//     );
+//   }
+// }
+
+// export default dashboard;
+
+
+// Server side api fetching end 
+
+
+
 import { db } from './firbaseconfig';
 import { collection, getDocs } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
@@ -305,24 +431,41 @@ const dashboard: any = async () => {
     const apiData = await getData();
     return (
       <>  
-      <div className='centerpage'>
-        <h1 className="text-center ">Firebase Data</h1>
-        <h1 className="text-center p-10"> <Link href="/Pages/Profile">Profile</Link></h1>
-        <div className="text-center ">
-          <form action="/search/">
-            <input
-              type="text"
-              name='search'
-              placeholder="Search"
-            />
-            <button type='submit'>Search</button>
+        <div className='centerpage'>
+          <div className="flex flex-col flex-1 justify-between md:flex-row ms:flex-col mb-6">
+            <div className="job-heading-card">
+              <h2 className="job-count-heading">Over 2,000+ part-time jobs available in</h2>
+              <div><span className="job-location-heading">Egmore, Chennai</span> <Link style={{ color:"#2523CA", fontWeight:500, fontSize:"16px", textDecorationLine:"underline"}} href="#">Change</Link></div>
+            </div>
+            <div className="w-full md:w-[370px]">
+              
+              <form className="max-w-md mx-auto" action="/search/">   
+                  <div className="relative">
+                      <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                       <Image src="/icon/ion_search.svg" width={24} height={24} alt="Search" />
+                      </div>
+                      <input type="search" id="default-search" className="global-search-bar block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-50 dark:border-gray-400 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search by locality, job type, company" required />
+                      {/* <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button> */}
+                  </div>
+              </form>
 
-          </form>
+              {/* <form action="/search/">
+                <input
+                  type="text"
+                  name='search'
+                  placeholder="Search"
+                />
+                <button type='submit'>Search</button>
 
+               </form> */}
+            </div>
+          </div>
+        <div className="job-list-wrpper">
+          <h1 className='job-list-wrpper-title'>New & Popular jobs</h1>
           {apiData.map((item: any, index: any) => (
             <div key={index}>
-              <div className="job-wrpper" key={item.id}>
-                <div className="job-wrpper" key={item.id}>
+              <div className="" key={item.id}>
+                <div className="job-list-card" key={item.id}>
                   <Link href={`Pages/Jobs/Job-details/${item.id}`}>
                     <div className="job-header">
                       {item.image ? (
@@ -335,21 +478,26 @@ const dashboard: any = async () => {
                         <span>{item.title}</span>
                       </div>
                     </div>
-                    <div className="job-body">
-                      <Image src="/icon/map-pin.svg" width={16} height={16} alt="Logo" />
-                      <span> {item.location}</span>
-                    </div>
-                    <div className="job-body">
-                      <Image src="/icon/clock.svg" width={16} height={16} alt="Logo" />
-                      <span>{formatTimeRange(item.start_time, item.end_time)}  {item.working_days}</span>
-                    </div>
-                    <div className="job-body">
-                      <Image src="/icon/wallet.svg" width={16} height={16} alt="Logo" />
-                      <span>₹ {item.start_salary} - {item.end_salary} per month</span>
-                    </div>
-                    <div className="postby">
-                      <span>{formatPostedTime(item.publish_time)}</span>
-                    </div>
+                    <div className="flex flex-col flex-1 md:flex-row ms:flex-col">
+                      <div className="flex flex-col flex-1 md:flex-row ms:flex-col">
+                        <div className="hidden md:block w-10"></div>
+                        <div className="job-body">
+                          <Image src="/icon/map-pin.svg" width={16} height={16} alt="Logo" />
+                          <span> {item.location}</span>
+                        </div>  
+                        <div className="job-body">
+                          <Image src="/icon/clock.svg" width={16} height={16} alt="Logo" />
+                          <span>{formatTimeRange(item.start_time, item.end_time)}  {item.working_days}</span>
+                        </div>
+                        <div className="job-body">
+                          <Image src="/icon/wallet.svg" width={16} height={16} alt="Logo" />
+                          <span>₹ {item.start_salary} - {item.end_salary} per month</span>
+                       </div>                          
+                      </div>
+                      <div className="postby">
+                        <span>{formatPostedTime(item.publish_time)}</span>
+                      </div>
+                    </div>                    
                   </Link>
                 </div>
               </div>
